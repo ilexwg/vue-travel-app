603bf398123b6d2394305b929b17670851047ad1
ilexwg <ilexwg@gmail.com>
2021-03-15 22:50:08 +0800

    Add Brzail page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Brazil from '@/views/Brazil.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/brazil',
+    name: 'brazil',
+    component: Brazil
+  }
 ]
 
 const router = new VueRouter({
